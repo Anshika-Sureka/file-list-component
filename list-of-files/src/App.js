8484716509bcd6ebe99d54e75dfc6c44a12bd386
Anshika-Sureka <anshika5.sureka5@gmail.com>
2022-05-13 10:18:07 -0700

    initial update

--- list-of-files/src/App.js
+++ list-of-files/src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import FileListComponent from './components/file-list-component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+      List of Files
       </header>
+      <div className="app-container">
+      
+        <FileListComponent/>
+      </div>
     </div>
   );
 }
